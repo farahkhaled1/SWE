3d617251c8c6e9130c05b623136c60cebef4be19
Saja Saadoun <saja1904826@miuegypt.edu.eg>
2022-05-25 19:18:56 +0200

    vp

---
+++ resources/views/viewProfile.blade.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Profile">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>View Profile</title>
+    <link rel="stylesheet" href="{{url('css/course-specs4-7.css')}}" media="screen">
+<link rel="stylesheet" href="{{url('css/View-Profile.css')}}" media="screen">
+    <script class="u-script" type="text/javascript" src="{{url('js/jquerycourse-specs.js')}}" defer=""></script>
+    <script class="u-script" type="text/javascript" src="{{url('js/nicepage.js')}}" defer=""></script>
+    <meta name="generator" content="Nicepage 4.7.1, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|PT+Sans:400,400i,700,700i|Allerta:400|PT+Sans+Caption:400,700">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/logo.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="View Profile">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode"><header class="u-clearfix u-header u-header" id="sec-a93c"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-opacity u-opacity-80 u-image-1" data-image-width="654" data-image-height="194">
+          <img src="{{url('images/logo.png')}}" class="u-logo-image u-logo-image-1">
+        </a>
+      </div></header>
+    <section class="u-align-center u-clearfix u-image u-section-1" id="carousel_c767" data-image-width="1980" data-image-height="1320">
+      <div class="u-clearfix u-sheet u-valign-bottom-sm u-valign-bottom-xs u-valign-middle-lg u-valign-middle-md u-valign-middle-xl u-sheet-1">
+        <h2 class="u-text u-text-1">Profile</h2>
+        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+          <div class="u-gutter-0 u-layout">
+            <div class="u-layout-row">
+              <div class="u-align-center-sm u-align-center-xs u-align-left-lg u-align-left-md u-align-left-xl u-container-style u-layout-cell u-size-28 u-layout-cell-1">
+                <div class="u-container-layout u-container-layout-1">
+                  <h4 class="u-custom-font u-text u-text-2">Personal Information</h4>
+                  <p class="u-text u-text-3">
+                    <span style="font-weight: 700;">
+                      <span style="font-size: 1.125rem;">Name</span>: 
+                    </span>
+                    <span style="font-weight: 400;">
+                      <span style="font-size: 1.125rem;">
+                        <br>Prof. Alaa Hamdy
+                      </span>
+                      <br><b>
+                        <span style="font-size: 1.125rem;">Courses</span>:</b>
+                      <br>
+                    </span>
+                    <span style="font-weight: 700;">
+                      <span style="font-weight: 400;">
+                        <span style="font-size: 1.125rem;">I</span>
+                        <span style="font-size: 1.125rem;">mage Processing&nbsp;</span>
+                      </span>
+                      <span style="font-size: 1.125rem;"></span>
+                    </span>
+                    <span style="font-size: 1.125rem;">
+                      <br>Opearting System 2
+                    </span>
+                    <br>
+                    <span style="font-weight: 700;">
+                      <span style="font-size: 1.125rem;">Email</span>:
+                    </span>
+                    <br>
+                    <span style="font-size: 1.125rem;">A</span>
+                    <span style="font-size: 1.125rem;">laahamdy@miuegypt.edu.eg</span>
+                    <br>
+                    <span style="font-weight: 700;">
+                      <span style="font-size: 1.125rem;">Misr</span>
+                      <span style="font-size: 1.125rem;">International</span>
+                      <span style="font-size: 1.125rem;">University</span>&nbsp;
+                    </span>
+                    <br>
+                  </p>
+                </div>
+              </div>
+              <div class="u-container-style u-custom-color-2 u-layout-cell u-size-32 u-layout-cell-2">
+                <div class="u-container-layout u-container-layout-2">
+                  <div class="u-image u-image-circle u-preserve-proportions u-image-1" alt="" data-image-width="700" data-image-height="774"></div>
+                  <h3 class="u-align-center u-custom-font u-text u-text-4"> Prof. Alaa hamdy</h3>
+                  <p class="u-align-center u-custom-font u-font-pt-sans u-text u-text-5"> Computer Science Professor&nbsp;</p>
+                  <a href="https://nicepage.com" class="u-active-palette-2-base u-border-3 u-border-active-palette-2-base u-border-custom-color-2 u-border-hover-palette-2-base u-btn u-btn-round u-button-style u-hover-custom-color-2 u-radius-50 u-text-active-white u-text-black u-text-hover-white u-white u-btn-1">Edit Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-e2a1"><div class="u-align-left u-clearfix u-sheet u-sheet-1"></div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  </body>
+</html>
